307dcaa8d5d7402e59cacc37fb5a99d46317b253
Desiree <desireeusifo@hotmail.com>
2020-09-20 09:35:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+
+//Array of Objects
+const quiz = [
+    {
+        q:"What date did the UK go to lockdown ?",
+        options:["March 6", "April 15", "March 23", "April 30" ],
+        answer: 0
+    },
+    {
+        q:"Who will Donald Trump stand against in the US presidential election?",
+        options:["Kamala Harris", "Joe Biden", "Bernie Sanders", "Hilary Clinton" ],
+        answer: 1
+    },
+    {
+        q:"Which film won Best Picture at the Oscars ?",
+        options:["Joker", "Once Upon A Time in Hollywood", "Parasite", "Judy" ],
+        answer: 2
+    },
+    {
+        q:"Which basketball star died in a helicopter crash ?",
+        options:["Michael Jordan", "Kobe Bryant", "LeBron James", "James Harden" ],
+        answer: 1
+    },
+    {
+        q:"In which Chinese city did the coronavirus start ?",
+        options:["Beijing", "Hong Kong", "Macau", "Wuhan" ],
+        answer: 3
+    },
+    {
+        q:"Who was the main character in the Netflix documentary Tiger King ?",
+        options:["Joe Exotic", "Joe Erotic", "Carole Baskin", "Carole Bascin" ],
+        answer: 0
+    },
+    {
+        q:"How many years in prision has sex offender Harvey Weinstein been sentenced to ?",
+        options:["23 years", "15 years", "32 years", "11 years" ],
+        answer: 0
+    },
+    {
+        q:"Which Iranian General was killed in a US drone strike in January ?",
+        options:["Commander-in-Chief of the IRGC Hossein Salami", "Quds Force top commander Qassem Soleimani", "Deputy Commander of the IRGC Ali Fadavi", "Commander of the Basij Resistance Force Gholamreza Soleimani" ],
+        answer: 1
+    },
+    {
+        q:"In what month did Prince Harry and Meghan Markle annonce they were stepping down as senior royals and what social-media platform did they use?",
+        options:["February via Twitter", "January via Instagram", "April via Snapchat", "March via TikTok" ],
+        answer: 1
+    },
+    {
+        q:"In what month was the first case of COVID-19 confirmed in the UK ?",
+        options:["January", "February", "March", "April" ],
+        answer: 0
+    },
+
+
+
+    
+
+]
